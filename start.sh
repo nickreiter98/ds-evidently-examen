59be747745491b4ba2aeca1ad41b9c415fdc066d
nickreiter98 <nick.reiter@hotmail.de>
2025-02-05 09:56:53 +0100

    Finalized automatic execution

--- start.sh
+++ start.sh
@@ -5,8 +5,9 @@ pip install -r requirements.txt
 
 python3 src/load_data.py
 python3 src/train_model.py
-python3 src/analyze_model_evaluation.py
+python3 src/analyze_model_validation.py
 python3 src/analyze_model_drift.py
-python3 src/analyze_concept_drift.py
+python3 src/analyze_target_drift.py
+python3 src/analyze_data_drift.py    
 
 evidently ui --workspace ./datascientest-workspace/ --host 0.0.0.0 --port 8888
